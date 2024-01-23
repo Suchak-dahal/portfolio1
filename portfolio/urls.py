8636f5c1fcfb1bb1436a3bb66553900e2cf5a503
Suchak Dahal <sharmasupreme111@gmail.com>
2024-01-23 18:48:12 +0545

    Home About Contact

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,6 @@ from portfolio import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('about', views.about, name = 'about'),
+    path('contact', views.contact, name = 'contact'),
 ]
